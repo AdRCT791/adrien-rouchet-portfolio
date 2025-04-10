3259d20e74ae16e425b98afd42af0bf24198c0d0
Adrien <adrien.rouchet@pm.me>
2025-04-10 13:08:17 +0200

    added logo.svg and updated BgFrame styles

--- src/components/BgFrame/BgFrame.tsx
+++ src/components/BgFrame/BgFrame.tsx
@@ -1,15 +1,22 @@
 import './BgFrame.css';
 import { siteData } from '../../data/content';
 import { useScrollProgress } from '../../hooks/useScrollProgress';
+import logo from '../../assets/logo.svg';
 
 const BgFrame = () => {
   const progress = useScrollProgress();
   return (
     <div className="bg-container">
-      <h1>{siteData.title.toUpperCase()}</h1>
-      <span style={{ display: progress >= 60 ? 'block' : 'none' }}>
-        {siteData.contact}
-      </span>
+      <div style={{ display: progress >= 60 ? 'none' : 'block' }}>
+        <img src={logo} alt="logo" className="bg-logo" />
+      </div>
+      <div
+        style={{ display: progress >= 60 ? 'flex' : 'none' }}
+        className="flex"
+      >
+        <h1>{siteData.title.toUpperCase()}</h1>
+        <span>{siteData.contact}</span>
+      </div>
     </div>
   );
 };
